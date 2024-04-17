d14ef2f6a47cff9bcc227b55872a45986ccef769
keeper521 <88678440+keeper521@users.noreply.github.com>
2024-04-17 11:44:34 +0300

    Update nginx-report.sh

--- lessons/BASH/nginx-report.sh
+++ lessons/BASH/nginx-report.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Конфигурация скрипта
 LOCK_FILE="/tmp/email_log_report.lock"
 LOG_FILE="/var/log/nginx/access.log"
 REPORT_FILE="/tmp/log_report_$(date +%Y%m%d%H%M%S).txt"
+TIMESTAMP_FILE="/tmp/email_log_report.timestamp"
 EMAIL="keeper521@yandex.ru"
 
-# Проверка на наличие файла блокировки для предотвращения одновременного выполнения
+# Создание файла блокировки для предотвращения одновременного запуска
 if [ -f "$LOCK_FILE" ]; then
     echo "Скрипт уже выполняется."
     exit 1
@@ -13,31 +15,43 @@ else
     touch "$LOCK_FILE"
 fi
 
-# Генерация отчёта
-echo "Отчёт о логах за последний час" > "$REPORT_FILE"
+# Получение времени последнего запуска, если оно есть
+if [ -f "$TIMESTAMP_FILE" ]; then
+    LAST_RUN=$(cat "$TIMESTAMP_FILE")
+else
+    LAST_RUN=$(date --date="1 hour ago" +"[%d/%b/%Y:%H:%M:%S")
+fi
+
+# Обновление времени последнего запуска скрипта
+NOW=$(date +"[%d/%b/%Y:%H:%M:%S")
+echo "$NOW" > "$TIMESTAMP_FILE"
+
+# Удаление лишнего символа в дате
+LAST=$(echo "$LAST_RUN" | sed 's/\[//g')
+NOW_DATA=$(echo "$NOW" | sed 's/\[//g')
+
+# Начало создания отчета
+echo "Отчёт о логах с $LAST  по $NOW_DATA" > "$REPORT_FILE"
 echo "=====================================" >> "$REPORT_FILE"
 
-# IP адреса с наибольшим количеством запросов
+# Извлечение и анализ данных из файла логов
 echo "IP адреса с наибольшим количеством запросов:" >> "$REPORT_FILE"
-awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5 >> "$REPORT_FILE"
+awk -v last_run="$LAST_RUN" -v now="$NOW" '$4 > last_run && $4 < now {print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5 >> "$REPORT_FILE"
 
-# Запрашиваемые URL
 echo "" >> "$REPORT_FILE"
-echo "Запрашиваемые URL с наибольшим количеством запросов:" >> "$REPORT_FILE"
-awk '{print $7}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5 >> "$REPORT_FILE"
+echo "URL с наибольшим количеством запросов:" >> "$REPORT_FILE"
+awk -v last_run="$LAST_RUN" -v now="$NOW" '$4 > last_run && $4 < now {print $7}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5 >> "$REPORT_FILE"
 
-# Ошибки сервера
 echo "" >> "$REPORT_FILE"
 echo "Ошибки сервера/приложения:" >> "$REPORT_FILE"
-awk '$9 ~ /^[45]/ {print $9,$7}' "$LOG_FILE" | sort | uniq -c | sort -nr >> "$REPORT_FILE"
+awk -v last_run="$LAST_RUN" -v now="$NOW" '$4 > last_run && $4 < now && $9 ~ /^[45]/ {print $9,$7}' "$LOG_FILE" | sort | uniq -c | sort -nr >> "$REPORT_FILE"
 
-# HTTP статус коды
 echo "" >> "$REPORT_FILE"
 echo "Коды HTTP ответов:" >> "$REPORT_FILE"
-awk '{print $9}' "$LOG_FILE" | sort | uniq -c | sort -nr >> "$REPORT_FILE"
+awk -v last_run="$LAST_RUN" -v now="$NOW" '$4 > last_run && $4 < now {print $9}' "$LOG_FILE" | sort | uniq -c | sort -nr >> "$REPORT_FILE"
 
 # Отправка отчёта
-mail -s "Отчёт о логах за последний час" "$EMAIL" < "$REPORT_FILE"
+mail -s "Отчёт о логах с $LAST по $NOW_DATA" "$EMAIL" < "$REPORT_FILE"
 
 # Удаление файла блокировки
 rm "$LOCK_FILE"
